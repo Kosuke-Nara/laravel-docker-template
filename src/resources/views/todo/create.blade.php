bb0d2e173db55aedf1092974ea212691a1dd4354
nara kosuke <narakosuke@naranoMacBook-Pro.local>
2025-05-14 18:12:27 +0900

    Controllerのリファクタリング

--- src/resources/views/todo/create.blade.php
+++ src/resources/views/todo/create.blade.php
@@ -1,6 +1,6 @@
 
-        @extends('layouts.base');
-        @section('content');
+        @extends('layouts.base')
+        @section('content')
         <div class="row justify-content-center">
           <div class="col-md-8">
             <div class="card">
