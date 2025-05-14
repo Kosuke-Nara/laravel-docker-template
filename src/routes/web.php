1d3bafbe337864fff4bc93c59ca42f67f54aea56
nara kosuke <narakosuke@naranoMacBook-Pro.local>
2025-05-14 10:53:00 +0900

    一覧画面のURIの設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/todo', function () {
+    echo 'Hello World!';
 });
