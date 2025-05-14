5206e12a5cbccd9afaa89b70c08ea57f39d54353
nara kosuke <narakosuke@naranoMacBook-Pro.local>
2025-05-14 12:31:32 +0900

    取得したデータの表示機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     public function index()
     {
         $todo = new Todo();
         $todos = $todo->all();
-        return view('todo.index');
+        
+        return view('todo.index', ['todos' => $todos]);
     }
 };
