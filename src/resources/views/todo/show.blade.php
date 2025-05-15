41a175267dc21eef5a9434accedf67e99625c29e
nara kosuke <narakosuke@naranoMacBook-Pro.local>
2025-05-15 12:37:41 +0900

    削除機能の実装

--- src/resources/views/todo/show.blade.php
+++ src/resources/views/todo/show.blade.php
@@ -2,20 +2,30 @@
 @section('content')
 <div class="row justify-content-center">
   <div class="col-md-8">
-    <div class="card">
+    <class="card">
       <div class="card-header">
         ToDo詳細
       </div>
+
       <div class="card-body">
         <h5 class="card-title">{{ $todo->content }}</h5>
         <p class="card-text">作成日時：{{ $todo->created_at }}</p>
       </div>
+
       <div class="row">
         <div class="col-auto">
           <a href="{{ route('todo.edit', $todo->id) }}" class="btn btn-info">編集する</a>
         </div>
       </div>
+
+    <div class="col-auto">
+      <form action="{{ route('todo.delete', $todo->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-danger">削除する</button>
+      </form>
     </div>
+
   </div>
 </div>
 @endsection 
